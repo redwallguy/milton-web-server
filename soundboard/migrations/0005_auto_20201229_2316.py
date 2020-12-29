6099182769bc7b169d1c6ca22d348c878b6cf479
Dev Vibhu Someshwar <redwallguy@gmail.com>
2020-12-29 18:29:54 -0500

    Manually removed migration dependency

--- soundboard/migrations/0005_auto_20201229_2316.py
+++ soundboard/migrations/0005_auto_20201229_2316.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('soundboard', '0004_clip_volume'),
+        ('soundboard'),
     ]
 
     operations = [
